f1d0a8316d592f66a7b4e7ffb39e4a99af464cc5
Shuhei Nishizawa <shuhey.dev@gmail.com>
2019-05-01 12:18:19 +0900

    プロジェクト ファイルを追加します。

---
+++ DIContainerPractice/App.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+
+
+namespace DIContainerPractice
+{
+    public static class App
+    {
+        public static UnityContainer Container { get; } = new UnityContainer();
+
+    }
+}
---
+++ DIContainerPractice/Program.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Ioc;
+using System;
+using System.Collections.Generic;
+using Unity;
+using Unity.Injection;
+
+
+namespace DIContainerPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+    class A
+    {
+        public void Do()
+        {
+            var b = new B();
+            var c = new C();
+            var d = new D();
+
+            b.Do();
+            c.Do();
+            d.Do();
+        }
+    }
+
+    class B
+    {
+        public void Do()
+        { }
+    }
+
+    class C
+    {
+        public void Do()
+        { }
+    }
+
+    class D
+    {
+        public void Do()
+        { }
+    }
+
+}
